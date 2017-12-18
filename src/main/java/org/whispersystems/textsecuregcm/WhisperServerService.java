c3651bdf7be019f9cc16138abbb2116c0f4f2daf
sadekyo1712 <buiduchung.bkhn@gmail.com>
2017-12-18 17:13:34 +0700

    update own minio

--- src/main/java/org/whispersystems/textsecuregcm/WhisperServerService.java
+++ src/main/java/org/whispersystems/textsecuregcm/WhisperServerService.java
@@ -201,6 +201,8 @@ public class WhisperServerService extends Application<WhisperServerConfiguration
     MessageController    messageController    = new MessageController(rateLimiters, pushSender, receiptSender, accountsManager, messagesManager, federatedClientManager);
     ProfileController    profileController    = new ProfileController(rateLimiters , accountsManager, config.getProfilesConfiguration());
 
+    environment.jersey().setUrlPattern("/api/*");
+    environment.jersey().getResourceConfig().setUrlPattern("/api/*");
     environment.jersey().register(new AuthDynamicFeature(new BasicCredentialAuthFilter.Builder<Account>()
                                                              .setAuthenticator(deviceAuthenticator)
                                                              .setPrincipal(Account.class)
